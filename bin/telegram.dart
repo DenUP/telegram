57d633ad523cf09070dc9d0f59be975a15d2b606
DenUP <ctrike163@gmail.com>
2024-12-03 19:56:43 +0300

    fix: try and catch

--- bin/telegram.dart
+++ bin/telegram.dart
@@ -41,6 +41,7 @@ final celevoeMenu = InlineMenu(name: "Celevoe Menu").text(
 /// Подгрузка бота
 void main() async {
   // Начала бота
+
   bot.attachMenu(startMenu);
 
   // Старт
@@ -70,27 +71,31 @@ void main() async {
 Future<void> sposobPadichiCallBack(Context ctx) async {
   if (intButton == 1) return;
   intButton = 1;
-  if (ctx.update.callbackQuery == false) return;
-  await ctx.editMessageText(
-    """<b>Способы подачи документов:</b>  \n
+  try {
+    await ctx.editMessageText(
+      """<b>Способы подачи документов:</b>  \n
 📌Лично в Центральную приемную комиссию или в отборочные комиссии факультетов.\n
 📌Адреса доступны на сайте ППТ в разделе <a href="https://www.ppt52.ru/kontakty1.html">"Поступающему"</a>\n 
 📌Единый портал государственных услуг \n
 📌Почтой России (куда: 607762, Нижегородская область, город Первомайск, улица Юбилейная, дом 24) """,
-    parseMode: ParseMode.html,
-    replyMarkup: startMenu,
-  );
+      parseMode: ParseMode.html,
+      replyMarkup: startMenu,
+    );
 
-  // Удаление меню, после нажатия
-  // bot.removeMenu(startMenu);
+    // Удаление меню, после нажатия
+    // bot.removeMenu(startMenu);
+  } catch (e) {
+    print('Ошибка - ${e}');
+  }
 }
 
 // Документы для поступления
 Future<void> documentCallBack(Context ctx) async {
   if (intButton == 2) return;
   intButton = 2;
-  await ctx.editMessageText(
-    """<b>Документы для поступления:</b>  \n
+  try {
+    await ctx.editMessageText(
+      """<b>Документы для поступления:</b>  \n
 1. <b>Заявление о поступлении</b> - заполняется в установленной форме.
 2. <b>Аттестат об основном общем образовании или о среднем общем образовании</b> - копия или оригинал.
 3. <b>Справка об успешном окончании 9-го или 11-го класса</b> (при необходимости).
@@ -99,10 +104,12 @@ Future<void> documentCallBack(Context ctx) async {
 6. <b>Фотографии</b> - паспортные фотографии (количество уточнить в учебном заведении).
 7. <b>Документы, подтверждающие льготы (если есть)</b> - для поступления на льготных условиях.
 """,
-    parseMode: ParseMode.html,
-    replyMarkup: startMenu,
-  );
-
+      parseMode: ParseMode.html,
+      replyMarkup: startMenu,
+    );
+  } catch (e) {
+    print('Ошибка - ${e}');
+  }
   // Удаление меню, после нажатия
   // bot.removeMenu(startMenu);
 }
@@ -111,15 +118,18 @@ Future<void> documentCallBack(Context ctx) async {
 Future<void> celevoeObychenieCallBack(Context ctx) async {
   if (intButton == 3) return;
   intButton = 3;
-  print(ctx.id.id);
-  await ctx.reply(
-    """<b>Целевое обучение:</b>  \n
+  try {
+    await ctx.reply(
+      """<b>Целевое обучение:</b>  \n
 Крутой нас """,
-    parseMode: ParseMode.html,
-    replyMarkup: celevoeMenu,
-  );
-  bot.removeMenu(startMenu);
-  // Удаление меню, после нажатия
+      parseMode: ParseMode.html,
+      replyMarkup: celevoeMenu,
+    );
+    bot.removeMenu(startMenu);
+    // Удаление меню, после нажатия
+  } catch (e) {
+    print('Ошибка - ${e}');
+  }
 }
 
 // Сроки подачи документов
@@ -127,8 +137,9 @@ Future<void> celevoeObychenieCallBack(Context ctx) async {
 Future<void> srokipodachidocumentovCallBack(Context ctx) async {
   if (intButton == 4) return;
   intButton = 4;
-  await ctx.editMessageText(
-    """<b>Сроки подачи документов:</b>  \n
+  try {
+    await ctx.editMessageText(
+      """<b>Сроки подачи документов:</b>  \n
 ✏️ Прием документов на очную, очно-заочную формы обучения
 начинается <b>20 июня 2024 г.</b>
 ✏️ Прием документов на очную форму обучения завершается <b>15 августа 2024г.</b>
@@ -139,11 +150,14 @@ Future<void> srokipodachidocumentovCallBack(Context ctx) async {
 ✏️ При наличии свободных мест прием документов на очно-заочную форму
 обучения осуществляется до <b>1 декабря 2024 г.</b> включительно.
 """,
-    parseMode: ParseMode.html,
-    replyMarkup: startMenu,
-  );
-  // bot.removeMenu(startMenu);
-  // Удаление меню, после нажатия
+      parseMode: ParseMode.html,
+      replyMarkup: startMenu,
+    );
+    // bot.removeMenu(startMenu);
+    // Удаление меню, после нажатия
+  } catch (e) {
+    print('Ошибка - ${e}');
+  }
 }
 
 // Контакты
@@ -151,8 +165,9 @@ Future<void> srokipodachidocumentovCallBack(Context ctx) async {
 Future<void> kontaktyCallBack(Context ctx) async {
   if (intButton == 5) return;
   intButton = 5;
-  await ctx.editMessageText(
-    """<b>Контакты:</b>  \n
+  try {
+    await ctx.editMessageText(
+      """<b>Контакты:</b>  \n
 🏡 Место нахождения/почтовый адрес: 607762, Нижегородская область, город Первомайск, улица Юбилейная, дом 24 \n
 📞 Телефон/Факс: (83139) 2-33-13, 2-33-23 \n
 ✉️ Адрес электронной почты: ppt_suz@mail.52gov.ru \n
@@ -161,26 +176,33 @@ Future<void> kontaktyCallBack(Context ctx) async {
 ⭐️ Мы - <a href="https://vk.com/club32513641">"ВКонтакте"</a>
 ⭐️ Мы - <a href="https://t.me/+LJqOBeoBssg3Mzgy">"Общий чат (Телеграм)"</a>
 """,
-    parseMode: ParseMode.html,
-    replyMarkup: startMenu,
-  );
-  // bot.removeMenu(startMenu);
-  // Удаление меню, после нажатия
+      parseMode: ParseMode.html,
+      replyMarkup: startMenu,
+    );
+    // bot.removeMenu(startMenu);
+    // Удаление меню, после нажатия
+  } catch (e) {
+    print('Ошибка - ${e}');
+  }
 }
 
 // Общий чат
 Future<void> obshiichatCallBack(Context ctx) async {
   if (intButton == 6) return;
   intButton = 6;
-  await ctx.editMessageText(
-    """<b>Общий чат:</b>  \n
+  try {
+    await ctx.editMessageText(
+      """<b>Общий чат:</b>  \n
 <a href="https://t.me/+LJqOBeoBssg3Mzgy">"Вступить в общий чат"</a>
 """,
-    parseMode: ParseMode.html,
-    replyMarkup: startMenu,
-  );
-  // bot.removeMenu(startMenu);
-  // Удаление меню, после нажатия
+      parseMode: ParseMode.html,
+      replyMarkup: startMenu,
+    );
+    // bot.removeMenu(startMenu);
+    // Удаление меню, после нажатия
+  } catch (e) {
+    print('Ошибка - ${e}');
+  }
 }
 
 // When a user clicks on the "Second" button, the bot will edit the message
